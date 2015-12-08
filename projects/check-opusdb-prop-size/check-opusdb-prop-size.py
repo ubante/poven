8af81cd0eb9c506e269c010f62d243fdd2e1c1b2
ubante <ubante@yahoo-inc.com>
2015-12-07 20:32:27 -0800

    Hodor

---
+++ projects/check-opusdb-prop-size/check-opusdb-prop-size.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import random
+
+"""
+This will write to the scratch table for now.
+#
+CREATE TABLE scratch (
+   k VARCHAR(255) UNIQUE,
+   v BLOB
+);
+
+The format will be:
+INSERT INTO scratch VALUES ('size-opsdb_prop_name', 'number of nodes')
+"""
+
+
+
+# Main
+print "Checking nodecount on a bunch of properties."
+
+
+# First read from opusdb to get the list of OpsDB properties.
+# http://opusdb.corp.yahoo.com:4080/v1/kvps?key=prop_dashboard_opsdb_names
+# That row is updated by munge-property-dashboard.sh
+OPSDBPROPLISTURL = "http://opusdb.corp.yahoo.com:4080/v1/kvps?key=prop_dashboard_opsdb_names"
+
+data = urllib.urlopen(OPSDBPROPLISTURL).read()
+jsondata = json.loads(data)
+# print "data:"
+# print data
+# print
+#
+# print "json data:"
+# print jsondata
+# print
+#
+# print "with a for-loop:"
+
+# Doing this in a loop because I can't figure out how to get the dictionary value of a dictionary value ??
+propertyliststring = ""
+# counter = 0
+for row in jsondata:
+    # counter = counter + 1
+    # print "%d: %s" % (counter, row[u'value'])
+    propertyliststring = row[u'value']
+
+# print "List of props: ", propertyliststring
+
+# Then loop through that list
+for property in propertyliststring.strip(',').split(','):
+
+    # Find the nodecount of that property
+    # XXX use a random number for now and later, use /home/y/bin/opsdb
+    count = "%1.0f" % ( random.random()*1000 )
+    print "--> ", property, count
+
+    # Write it to OpusDB
+    SCRATCHENDPOINTPREFIX = "http://opusdb.corp.yahoo.com:4080/v1/scratch/upsert?key="
+    update_url = SCRATCHENDPOINTPREFIX + "size-" + property + "&value=" + count
+    # exit(1)
+    print update_url
+    urllib.urlopen(update_url).read()
+
+
+
+
+
+
+
+
+
