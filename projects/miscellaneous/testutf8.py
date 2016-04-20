d929271d55a0add01249518cb4e48a1eb64844ed
John Ubante <condoti@yahoo.com>
2016-04-19 17:19:31 -0700

    UTF8 is great

---
+++ projects/miscellaneous/testutf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Found in http://blog.manbolo.com/2014/09/27/use-python-effectively-on-os-x
+
+If UTF-8 is not set, this will spit out:
+SyntaxError: Non-ASCII character '\xe2' in file /Users/john.ubante/PycharmProjects/poven/projects/miscellaneous/testutf8.py
+on line 8, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details
+"""
+
+print("Hello こんにちは!")
