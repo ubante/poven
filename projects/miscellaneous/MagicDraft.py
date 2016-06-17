53ee08f75dad72022889d1c5be2160a93a72ad41
John Ubante <ubante@yahoo-inc.com>
2016-06-17 00:33:42 -0700

    Got a table of players

--- projects/miscellaneous/MagicDraft.py
+++ projects/miscellaneous/MagicDraft.py
@@ -56,6 +56,7 @@ class CardSet:
             print "%3s" % card.get_description(),
         print ""
 
+
 class Player:
     """
     I play
@@ -77,8 +78,89 @@ class Player:
             print "%3s" % card.get_description(),
         print ""
 
+
+class DoubleList(object):
+    """
+    Thank you, Mr. Parker.
+    http://ls.pwd.io/2014/08/singly-and-doubly-linked-lists-in-python/
+    """
+    head = None
+    tail = None
+
+    def append(self, data):
+        new_node = Chair(data, None, None)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            new_node.nexxt = None
+            self.tail.nexxt = new_node
+            self.tail = new_node
+
+    def remove(self, node_value):
+        current_node = self.head
+
+        while current_node is not None:
+            if current_node.data == node_value:
+                # if it's not the first element
+                if current_node.prev is not None:
+                    current_node.prev.nexxt = current_node.nexxt
+                    current_node.nexxt.prev = current_node.prev
+                else:
+                    # otherwise we have no prev (it's None), head is the next one, and prev becomes None
+                    self.head = current_node.nexxt
+                    current_node.nexxt.prev = None
+
+            current_node = current_node.nexxt
+
+    def show(self):
+        print "Show list data:"
+        current_node = self.head
+        while current_node is not None:
+            print current_node.prev.data if hasattr(current_node.prev, "data") else None,
+            print current_node.data,
+            print current_node.nexxt.data if hasattr(current_node.nexxt, "data") else None
+
+            current_node = current_node.nexxt
+        print "*"*50
+
+
+class Chair(object):
+    """
+    Maybe we should just use Player and give it prev and next fields.
+    """
+
+    def __init__(self, data, prev, nexxt):
+        self.data = data
+        self.prev = prev
+        self.nexxt = nexxt
+
+
 class Table:
-    pass
+    """
+    Thing the players sit at
+    """
+
+    def __init__(self, player_count, round_count, card_max_value, deck_size, card_colors):
+        self.player_count = player_count
+        self.round_count = round_count
+        self.card_max_value = card_max_value
+        self.deck_size = deck_size
+        self.card_colors = card_colors
+        self.players = []
+
+    def initialize(self):
+        for i in range(self.player_count):
+            self.players.append(Player(self.deck_size, self.card_colors, self.card_max_value))
+
+    def display(self):
+        print "Here's the table of %d players:" % self.player_count
+        for i in range(0, self.player_count):
+            print "Player #%d:" % i
+            self.players[i].print_deck()
+            self.players[i].print_hand()
+
+
 
 def main(argv):
     """
@@ -103,6 +185,12 @@ def main(argv):
     aPlayer = Player(deck_size, card_colors, card_max_value)
     aPlayer.print_deck()
     aPlayer.print_hand()
+
+    print
+    aTable = Table(player_count, round_count, card_max_value, deck_size, card_colors)
+    aTable.initialize()
+    aTable.display()
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
