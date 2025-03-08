94e21bd92cc658804fc2f07c233aca79438452d5
John Ubante <ubante@users.noreply.github.com>
2025-03-07 22:46:24 -0800

    Finished sim - takes 281 packs for a solid collection.
    Seems high.  Would be interesting to see how many fewer
    packs are needed if one dusts the excess cards to create
    the missing cards.

--- projects/hearthstone/src/common.py
+++ projects/hearthstone/src/common.py
@@ -1,11 +1,14 @@
 """
 I commonly put common things here.
 """
+import logging
+import random
 from collections import defaultdict
 from enum import Enum
 
 from projects.hearthstone.src.constants import PITY_EPIC, PITY_LEGENDARY, GUARANTEED_LEGENDARY, RAPTOR1_COUNT_COMMON, \
-    RAPTOR1_COUNT_LEGENDARY, RAPTOR1_COUNT_RARE, RAPTOR1_COUNT_EPIC
+    RAPTOR1_COUNT_LEGENDARY, RAPTOR1_COUNT_RARE, RAPTOR1_COUNT_EPIC, PACK_SIZE, CARD_DROP_RATE_LEGENDARY, \
+    CARD_DROP_RATE_EPIC, CARD_DROP_RATE_RARE
 
 
 class Rarity(Enum):
@@ -13,19 +16,6 @@ class Rarity(Enum):
     RARE = 2
     EPIC = 3
     LEGENDARY = 4
-
-
-class Pack(object):
-    """
-    These are the things you earn via rewards or buy via dollars.  The contents
-    depend on player collection.  That is, packs will not contain a third copy
-    of a card until the player has a complete set of that card's rarity.
-    
-    I'll have to make some assumptions since the logic isn't public.
-    """
-
-    def __init__(self):
-        self.cards: list[Card] = []
 
 
 class Card(object):
@@ -38,14 +28,52 @@ class Card(object):
         self.rarity: Rarity = rarity
         self.dust_value: int = 0
 
-    @staticmethod
-    def random():  # strange that I can't use Card as a hint
-        """
-        This should be a random card irrespective of the player's collection.
-        :return:
-        """
-        random_card = Card("name33", 33, Rarity.COMMON)
-        return random_card
+    def __str__(self):
+        return f"{self.name}, {self.index}, {self.rarity}"
+
+
+class Pack(object):
+    """
+    These are the things you earn via rewards or buy via dollars.  The contents
+    depend on player collection.  That is, packs will not contain a third copy
+    of a card until the player has a complete set of that card's rarity.
+
+    I'll have to make some assumptions since the logic isn't public.
+    """
+
+    def __init__(self):
+        self.cards: list[Card] = []
+
+    def add(self, card: Card):
+        self.cards.append(card)
+
+    def size(self) -> int:
+        return len(self.cards)
+
+    def open(self) -> list[Card]:
+        return self.cards
+
+    def contains(self, rarity: Rarity) -> bool:
+        for card in self.cards:
+            if card.rarity == rarity:
+                return True
+
+        return False
+
+    def contains_only_commons(self):
+        for card in self.cards:
+            if card.rarity != Rarity.COMMON:
+                return False
+
+        return True
+
+    def get_rarities(self):
+        rarities = ""
+        for card in self.cards:
+            rarities += str(card.rarity.name)
+            rarities += " "
+
+        return rarities
 
 
 class Player(object):
@@ -63,8 +91,12 @@ class Player(object):
         self.pity_timer = {Rarity.EPIC: 0,
                            Rarity.LEGENDARY:0}
 
-    def get_pack(self):
-        pass
+    def open_pack(self):
+        pack = Store.buy_pack(self)
+        self.pack_count += 1
+        logging.debug(f"{pack.get_rarities()}")
+        for card in pack.open():
+            self.collection.add(card)
 
 
 class Collection(object):
@@ -106,12 +138,43 @@ class Collection(object):
     def add(self, card: Card):
         self.raptor1[card.rarity][card.index] += 1
 
+    def is_complete(self, rarity: Rarity) -> bool:
+        """
+        Returns true if the collection contains at least a pair of each card
+        at the given rarity.
+
+        :param rarity:
+        :return:
+        """
+        for index in self.raptor1[rarity]:
+            complete_count = 2
+            if rarity == Rarity.LEGENDARY:
+                complete_count = 1
+
+            if self.raptor1[rarity][index] < complete_count:
+                return False
+
+        return True
+
+    def is_fully_complete(self) -> bool:
+        """
+        Returns True if the collection has at least one card for each legendary
+        and a pair of every other card.
+
+        :return:
+        """
+        for r in Rarity:
+            if not self.is_complete(r):
+                return False
+
+        return True
+
     def is_solid(self) -> bool:
         """
         Returns true if player has a pair of each common, rare and epic.
         :return:
         """
-        for r in Rarity:
+        for r in [Rarity.COMMON, Rarity.RARE, Rarity.EPIC]:
             for index in self.raptor1[r]:
                 if self.raptor1[r][index] < 2:
                     return False
@@ -119,6 +182,11 @@ class Collection(object):
         return True
 
     def contains_legendary(self) -> bool:
+        """
+        Returns true if the collection has at least one legendary.
+
+        :return:
+        """
         for ind in self.raptor1[Rarity.LEGENDARY]:
             if self.raptor1[Rarity.LEGENDARY][ind] > 0:
                 return True
@@ -126,16 +194,53 @@ class Collection(object):
         return False
 
     def display(self):
-        print("\n-----------------------------------------------\n"
+        print("\n"
+              "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n"
               "Year of the Raptor.  Set 1 is 'Into the Emerald Dream'")
 
-        for r,d in self.raptor1.items():
-            print(f"{r}:")
+        for rarity, d in self.raptor1.items():
+            print(f"{rarity.name}:")
             for index in d:
                 print(f"{index:>3}:{d[index]:>2}", end=" ")
                 if not index % 10 and index != len(d):
                     print()
             print()
+
+    def print_stats(self):
+        all_count = 0
+        for rarity in Rarity:
+            rarity_count = 0
+            total_rarity_count = 0
+            rarity_slice = self.raptor1[rarity]
+            for index in rarity_slice.keys():
+                all_count += rarity_slice[index]
+                total_rarity_count += rarity_slice[index]
+                if rarity == Rarity.LEGENDARY:
+                    if rarity_slice[index]:
+                        rarity_count += 1
+                        continue
+                else:
+                    if rarity_slice[index] >= 2:
+                        rarity_count += 2
+                        continue
+                rarity_count += rarity_slice[index]
+
+            out_of = 0
+            match rarity:
+                case Rarity.COMMON:
+                    out_of = RAPTOR1_COUNT_COMMON * 2
+                case Rarity.RARE:
+                    out_of = RAPTOR1_COUNT_RARE * 2
+                case Rarity.EPIC:
+                    out_of = RAPTOR1_COUNT_EPIC * 2
+                case Rarity.LEGENDARY:
+                    out_of = RAPTOR1_COUNT_LEGENDARY
+
+            percentage = int(rarity_count / out_of * 100)
+
+            print(f"{rarity.name:>12}: {rarity_count:>3}/{out_of:<3} "
+                  f"{percentage}% {total_rarity_count} cards")
+        print(f"Collection has {all_count} cards.")
 
     def compact_display(self):
         self.compact_display_line(Rarity.COMMON)
@@ -162,30 +267,87 @@ class Store(object):
 
         # Check if player has hit 10 pack guarantee.
         if player.pack_count == (GUARANTEED_LEGENDARY - 1) and not player.collection.contains_legendary():
-            # Add a random legendary to pack
-            pass
+            logging.debug("Hit the 10 pack guarantee.")
+            pack.add(Store.get_random_card_of_rarity(player.collection, Rarity.LEGENDARY))
 
         # Check if the player has hit any pity timer.
         if player.pity_timer[Rarity.EPIC] == (PITY_EPIC - 1):
-            # Add an epic to pack
-
+            logging.debug("Hit the Epic pity timer.")
+            pack.add(Store.get_random_card_of_rarity(player.collection, Rarity.EPIC))
             player.pity_timer[Rarity.EPIC] = 0
 
         if player.pity_timer[Rarity.LEGENDARY] == (PITY_LEGENDARY - 1):
-            # Add an epic to pack
-
+            logging.debug("Hit the Legendary pity timer.")
+            pack.add(Store.get_random_card_of_rarity(player.collection, Rarity.LEGENDARY))
             player.pity_timer[Rarity.LEGENDARY] = 0
 
         # For the remaining cards, use the drop rates.
+        for i in range(1, PACK_SIZE - pack.size()):
+            pack.add(Store.get_random_card_of_rarity(player.collection, Store.decide_rarity()))
 
         # For the final card, if the previous four cards were uncommon, then
         # remove common cards from the random pool.
+        if pack.contains_only_commons():
+            pity = Rarity.COMMON
+            while pity == Rarity.COMMON:
+                pity = Store.decide_rarity()
+            pack.add(Store.get_random_card_of_rarity(player.collection, pity))
+        else:
+            pack.add(Store.get_random_card_of_rarity(player.collection, Store.decide_rarity()))
+
+        # Increment pity timers if necessary.
+        for rarity in [Rarity.LEGENDARY, Rarity.EPIC]:
+            if not pack.contains(rarity):
+                player.pity_timer[rarity] += 1
 
         return pack
 
     @staticmethod
-    def get_random_card(col: Collection, rarity: Rarity) -> Card:
-        pass
+    def decide_rarity() -> Rarity:
+        rnd = random.random()
+
+        if rnd < CARD_DROP_RATE_LEGENDARY:
+            return Rarity.LEGENDARY
+        elif rnd < CARD_DROP_RATE_LEGENDARY + CARD_DROP_RATE_EPIC:
+            return Rarity.EPIC
+        elif rnd < CARD_DROP_RATE_LEGENDARY + CARD_DROP_RATE_EPIC + CARD_DROP_RATE_RARE:
+            return Rarity.RARE
+
+        return Rarity.COMMON
+
+    @staticmethod
+    def get_random_card_of_rarity(col: Collection, rarity: Rarity) -> Card:
+        # This is the slice of the collection that contains cards of this rarity.
+        rarity_slice = col.raptor1[rarity]
+
+        # This will find all the cards that are not full
+        complete_count = 2
+        if rarity == Rarity.LEGENDARY:
+            complete_count = 1
+
+        available_indices = []
+        for index in rarity_slice.keys():
+            if rarity_slice[index] >= complete_count:
+                continue
+
+            available_indices.append(index)
+
+        # When this rarity is already complete, then we can return any card
+        # of that rarity.
+        if not available_indices:
+            available_indices = list(range(1, len(rarity_slice) + 1))
+
+        # Generate a random number.
+        rnd = random.randint(1, len(available_indices))
+        random_card_index = available_indices[rnd - 1]
+
+        return Card("random", random_card_index, rarity)
+
+
+
+
+
+
 
 
 
--- projects/hearthstone/tests/test_common.py
+++ projects/hearthstone/tests/test_common.py
@@ -3,6 +3,83 @@ Focus on testing important methods, like buy_pack().
 """
 from unittest import TestCase
 
+from projects.hearthstone.src.common import Collection, Card, Rarity, Store, Player
+
+
 class TestPlayer(TestCase):
     def test_is_complete(self):
-        self.fail()
+        pass
+
+
+class TestCollection(TestCase):
+    def setUp(self):
+        self.col = Collection()
+
+    def fill_rarity(self, rarity):
+        complete_count = 2
+        if rarity == Rarity.LEGENDARY:
+            complete_count = 1
+
+        for index in self.col.raptor1[rarity]:
+            self.col.raptor1[rarity][index] = complete_count
+
+    def test_is_complete(self):
+        # We'll use epics and assume the same results for other rarities.
+        # First test non-completeness.
+        self.assertFalse(self.col.is_complete(Rarity.EPIC))
+
+        # Fill that rarity and confirm completeness.
+        self.fill_rarity(Rarity.EPIC)
+        self.assertTrue(self.col.is_complete(Rarity.EPIC))
+
+    def test_is_fully_complete(self):
+        # First test non-fully-completeness.
+        self.assertFalse(self.col.is_fully_complete())
+
+        # Fill the collection and confirm that it is now fully complete.
+        for rarity in Rarity:
+            self.fill_rarity(rarity)
+        self.assertTrue(self.col.is_fully_complete())
+
+    def test_is_solid(self):
+        # First test the negation.
+        self.assertFalse(self.col.is_solid())
+
+        for rarity in [Rarity.COMMON, Rarity.RARE, Rarity.EPIC]:
+            self.fill_rarity(rarity)
+        self.assertTrue(self.col.is_solid())
+
+    def test_contains_legendary(self):
+        # First assert that the collection has no legendaries.
+        self.assertFalse(self.col.contains_legendary())
+
+        # Add a legendary card and confirm the change.
+        legend = Card("Test Legend", 10, Rarity.LEGENDARY)
+        self.col.add(legend)
+        self.assertTrue(self.col.contains_legendary())
+
+    def tearDown(self):
+        pass
+
+
+class TestStore(TestCase):
+    def setUp(self):
+        self.player = Player("unittest")
+
+    def test_buy_pack(self):
+        pack = Store.buy_pack(self.player)
+        self.assertEqual(len(pack.cards), 5)
+        self.assertFalse(pack.contains_only_commons())
+
+    def test_get_random_card(self):
+        common = Store.get_random_card_of_rarity(self.player.collection, Rarity.COMMON)
+        self.assertTrue(common.rarity, Rarity.COMMON)
+
+        rare = Store.get_random_card_of_rarity(self.player.collection, Rarity.RARE)
+        self.assertTrue(rare.rarity, Rarity.RARE)
+
+        epic = Store.get_random_card_of_rarity(self.player.collection, Rarity.EPIC)
+        self.assertTrue(epic.rarity, Rarity.EPIC)
+
+        legendary = Store.get_random_card_of_rarity(self.player.collection, Rarity.LEGENDARY)
+        self.assertTrue(legendary.rarity, Rarity.LEGENDARY)
